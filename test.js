5e184700def1a1c146387e32842f7a7fe85b1088
bigforo <bigforo@gmail.com>
2014-11-24 18:44:29 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,72 @@
 //
 //
+
+
+    $(document).ready(function () {
+        if (navigator.userAgent.match(/Android/i)) {
+            window.scrollTo(0, 1);
+        }
+        $('#bs0').backslider({
+            mode: 'timer',
+            effect: 'slidefade',
+            effectTime: 3000,
+            photoSource: 'picasa',
+            centerImages: true,
+            timer: 10000,
+            picasaQuery: {
+                user: '112973260543911044435',
+                album: '6014979933820203569'
+            }
+        });
+
+        $('#Gallery1').click(function () {
+            $('#bs0').remove();
+            $(document.body).append('<div id="bs0" class="backslider" style="z-index:-1;"></div>');
+            $('#bs0').backslider({
+                mode: 'timer',
+                effect: 'slidefade',
+                effectTime: 3000,
+                photoSource: 'picasa',
+                centerImages: true,
+                timer: 10000,
+                picasaQuery: {
+                    user: '112973260543911044435',
+                    album: '6014979933820203569'
+                }
+            });
+
+        });
+        $('#Gallery2').click(function () {
+            $('#bs0').remove();
+            $(document.body).append('<div id="bs0" class="backslider" style="z-index:-1;"></div>');
+            $('#bs0').backslider({
+                mode: 'timer',
+                effect: 'slidefade',
+                effectTime: 3000,
+                photoSource: 'picasa',
+                centerImages: true,
+                timer: 10000,
+                picasaQuery: {
+                    user: '112973260543911044435',
+                    album: '6027003481855779489'
+                }
+            });
+
+        });
+
+        addEventListener("click", function () {
+            var
+                  el = document.documentElement
+                , rfs =
+                       el.requestFullScreen
+                    || el.webkitRequestFullScreen
+                    || el.mozRequestFullScreen;
+            rfs.call(el);
+        });
+
+        $('body').on("click", function () {
+            var el = document.documentElement, rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen;
+            rfs.call(el);
+        });
+
+    });
